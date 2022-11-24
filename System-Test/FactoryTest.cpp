6fd07136a63652a2a2116dd123310f6ace51e873
游齐宇 <55417548+Ysofun@users.noreply.github.com>
2022-11-24 18:38:04 +0800

    Factory Test

--- System-Test/FactoryTest.cpp
+++ System-Test/FactoryTest.cpp
@@ -12,6 +12,10 @@ TEST(FactoryTest, staticConstruct)
 	EXPECT_STREQ(typeid(InputStrategyFactory::GetInstance()).name(), "class InputStrategyFactory");
 	EXPECT_STREQ(typeid(OutputStrategyFactory::GetInstance()).name(), "class OutputStrategyFactory");
 
-	ASSERT_TRUE(InputStrategyFactory::GetInstance().GetStrategy("Csv"));
-	ASSERT_TRUE(InputStrategyFactory::GetInstance().GetStrategy("Console"));
+	ASSERT_TRUE(dynamic_cast<InputCsvStrategy*>(InputStrategyFactory::GetInstance().GetStrategy("Csv")));
+	ASSERT_TRUE(dynamic_cast<InputConsoleStrategy*>(InputStrategyFactory::GetInstance().GetStrategy("Console")));
+	ASSERT_TRUE(dynamic_cast<InputJsonStrategy*>(InputStrategyFactory::GetInstance().GetStrategy("Json")));
+
+	ASSERT_TRUE(dynamic_cast<OutputCsvStrategy*>(OutputStrategyFactory::GetInstance().GetStrategy("Csv")));
+	ASSERT_TRUE(dynamic_cast<OutputHtmlStrategy*>(OutputStrategyFactory::GetInstance().GetStrategy("Html")));
 }
